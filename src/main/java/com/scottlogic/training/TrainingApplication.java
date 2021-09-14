bf2f4703c97062895be5013df84f11299da3b9cb
JJ Gray <jjgray@scottlogic.com>
2021-09-14 11:07:03 +0100

    removing use of matcher from top level code

--- src/main/java/com/scottlogic/training/TrainingApplication.java
+++ src/main/java/com/scottlogic/training/TrainingApplication.java
@@ -10,13 +10,6 @@ public class TrainingApplication {
         SpringApplication.run(TrainingApplication.class, args);
 
         System.out.println("hello");
-        Matcher matcher = new Matcher();
-        Order exampleOrder1 = new Order("jjgray", 100, 20, Direction.BUY);
-        Order exampleOrder2 = new Order("bpritchard", 100, 10, Direction.SELL);
-        matcher.receiveOrder(exampleOrder1);
-        System.out.println(matcher.state.getOrders());
-        matcher.receiveOrder(exampleOrder2);
-        System.out.println(matcher.state.getOrders());
     }
 
 }
