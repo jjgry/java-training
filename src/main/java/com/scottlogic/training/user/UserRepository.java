47de8d1d5d9bddc0c3424abd3f7a7a9c69e312be
JJ Gray <jjgray@scottlogic.com>
2021-09-16 16:20:03 +0100

    remove unused import

--- src/main/java/com/scottlogic/training/user/UserRepository.java
+++ src/main/java/com/scottlogic/training/user/UserRepository.java
@@ -1,6 +1,5 @@
 package com.scottlogic.training.user;
 
-import com.scottlogic.training.user.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
