c01be790ae735354bc3c0b61f4e3c7162b1f600d
JJ Gray <jjgray@scottlogic.com>
2021-09-27 09:59:21 +0100

    make OrdersDTO object compatible with frontend

--- src/main/java/com/scottlogic/training/order/OrdersDTO.java
+++ src/main/java/com/scottlogic/training/order/OrdersDTO.java
@@ -1,12 +1,11 @@
 package com.scottlogic.training.order;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class OrdersDTO {
-    public final List<OrderDTO> orders;
+    public final List<Order> orders;
 
     public OrdersDTO(List<Order> orders) {
-        this.orders = orders.stream().map(Order::toOrderDTO).collect(Collectors.toList());
+        this.orders = orders;
     }
 }
